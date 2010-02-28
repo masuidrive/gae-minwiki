1f6429c607395ad3fae58ad665e384f24123b9db
Yuichiro MASUI <masuidrive@masuidrive-mbp.local>
2010-02-28 12:34:09 -0800

    またサニタイズミス

--- assets/show.js
+++ assets/show.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	var newWikiPages = $("#content .new-page").map(function(i, el){return $(el).attr("title");});
 	var text = $("#content").text();
+	text = $('<div/>').text(text).html();
 	converter = new Showdown.converter();
 	$("#content").html(converter.makeHtml(text, newWikiPages));
 });
