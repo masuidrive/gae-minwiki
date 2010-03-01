1a90fa4c58364a7e24f8caaad30574f3d1e15664
Yuichiro MASUI <masuidrive@masuidrive-mbp.local>
2010-02-28 17:35:01 -0800

    favicon、robots.txt

--- dnsbl.py
+++ dnsbl.py
@@ -4,29 +4,19 @@ import socket
 import re
 
 def CheckSpamIP(check_ip = False):
-    # スパムIPアドレス
     spam_ip = "127.0.0.2"
-
-    # IPアドレスが不当な場合はスパムとみなす
-    # (単純にXXX.XXX.XXX.XXXかどうかだけチェック)
     if not re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}").match(check_ip):
         return True
-
-    # IPアドレス逆順並べ替え(AAA.BBB.CCC.DDD -> DDD.CCC.BBB.AAA)
     ip_rev = ".".join(reversed(check_ip.split(".")))
-
-    # 検索ドメイン生成
     host = "%s.dnsbl.spam-champuru.livedoor.com" % ip_rev
 
-    # ドメイン正引き
     try:
         ip = socket.gethostbyname(host)
-    except:                    # 正引き失敗時はスパム元IPアドレス以外と判断
+    except:
         return False
 
 
-    # 正引き後アドレス判定
-    if ip == spam_ip:          # スパム
+    if ip == spam_ip:
         return True
-    else:                      # スパム以外
+    else: 
         return False
